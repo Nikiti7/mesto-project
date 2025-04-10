d0525a5954367e00c1e59131f6819ab456678f0a
Nikolay Oleynik <nikolayoleynik7@mail.ru>
2025-04-10 19:31:12 +0300

    create a card template and take Six cards "out of the box"

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
 // @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const cardsContent = content.querySelector('.places__list');
+
+
+function addCard(name, link) {
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+	cardElement.querySelector('.card__image').src = link;
+	cardElement.querySelector('.card__title').textContent = name;
+
+	cardsContent.append(cardElement);
+}
 
 // @todo: DOM узлы
 
@@ -7,3 +20,6 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+	addCard(item.name, item.link);
+});
